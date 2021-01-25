190b44bc42da782af899aa413ed2ce3fdebcfd82
Joe Young <musicjoeyoung@gmail.com>
2021-01-25 18:04:13 -0500

    EDIT: updated practice room; PDF

---
+++ src/PDF.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function PDF() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="etude2.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
+export default PDF;
--- src/PracticeRoom.js
+++ src/PracticeRoom.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Metronome from "./Metronome";
+import PDF from "./PDF";
 
 export class PracticeRoom extends Component {
   render() {
     return (
-      <div>
+      <div id="musicStandDiv">
         <h1>Virtual Practice Room</h1>
         <img
           id="musicStand"
@@ -12,11 +14,9 @@ export class PracticeRoom extends Component {
             "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fa.1stdibscdn.com%2FarchivesE%2Fupload%2F1121189%2Ff_58962631478609847977%2F5896263_z.jpg&f=1&nofb=1"
           }
         />
-        <img
-          id="sheetMusic"
-          alt=""
-          src={"my-app/public/images/etude 2-page-001.jpg"}
-        />
+
+        <PDF />
+        <Metronome />
       </div>
     );
   }
